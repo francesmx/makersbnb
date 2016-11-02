37ac3071ee481c84d208e1273d6484260ac32802
michkles <michkles@gmail.com>
2016-11-02 18:52:57 +0000

    errors: moved models to public folder

---
+++ app/models/space.rb
@@ -0,0 +1,26 @@
+require 'data_mapper'
+require 'dm-postgres-adapter'
+require_relative 'request.rb'
+
+class Space
+  include DataMapper::Resource
+
+  has n, :requests, through: Resource
+
+  property :id,             Serial
+  property :name,           String
+  property :description,    Text
+  property :price,          Integer
+  property :available_from, Date
+  property :available_to,   Date
+
+  def self.search_availability(available_from, available_to)
+    all(:available_from.lte => available_from,
+        :available_to.gte => available_to)
+  end
+
+end
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/makersbnb_#{ENV['RACK_ENV']}")
+DataMapper.finalize
+DataMapper.auto_upgrade!
