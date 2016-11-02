b11571cacf2708713a325379560395d5dfed5690
Frances Maxwell <fmaxwell@gmail.com>
2016-11-02 16:16:24 +0000

    Incorporated JQueryUI datepicker functionality

--- app/public/js/interface.js
+++ app/public/js/interface.js
@@ -8,5 +8,10 @@ $(document).ready(function() {
     dateFormat: 'dd-mm-yy'
   });
 
+  $("#check_in, #check_out").datepicker({
+    dateFormat: 'dd-mm-yy',
+    minDate: new Date(2016, 11 - 1, 08),
+    maxDate: new Date(2016, 11 - 1, 15)
+  });
 
 });
