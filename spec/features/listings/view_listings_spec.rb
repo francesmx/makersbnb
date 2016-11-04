75631c2c029b2403b3b5414f09510eea82b43ce0
Frances Maxwell <fmaxwell@gmail.com>
2016-11-04 03:48:24 +0000

    Improved styling for spaces and space views. Refactored tests to make them pass again.

--- spec/features/listings/view_listings_spec.rb
+++ spec/features/listings/view_listings_spec.rb
@@ -9,7 +9,7 @@ describe 'View Listing' do
 
   it "can view a list of all available properties" do
     visit('/spaces')
-    expect(page).to have_content "Available Properties"
+    expect(page).to have_content "Book a Space"
     expect(page).to have_content 'Beautiful Relaxing Space'
     expect(page).to have_content 'have fun at our BnB'
   end
@@ -17,10 +17,10 @@ describe 'View Listing' do
   it "be able to filter properties by date availability" do
     visit('/spaces')
     expect(page).to have_content "Available from"
-    expect(page).to have_content "Available to"
+    expect(page).to have_content "to"
     fill_in :available_from, with: '2017/02/01'
     fill_in :available_to, with: '2017/02/10'
-    click_button 'Submit'
+    click_button 'List spaces'
     expect(page).to_not have_content "Old Space"
     expect(page).to have_content "New Space"
   end
